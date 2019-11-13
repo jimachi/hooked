c817dad32491f59168d2a39698993db31834037e
jimaichiro <54446706+YuichiroIijima@users.noreply.github.com>
2019-11-13 16:04:35 +0900

    add header.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = props => {
+  return (
+    <header clasname="App-header">
+      <header>{props.text}</header>
+    </header>
+  );
+};
+
+export default Header;
