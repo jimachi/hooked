045d84e990699ac0d66fc6695ecba9134151dc3c
jimaichiro <54446706+YuichiroIijima@users.noreply.github.com>
2019-11-15 00:54:17 +0900

    move some files

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useReducer } from "react";
-import "./App.css";
-import Header from "./components/Header";
-import Movie from "./components/Movie";
-import Search from "./components/Search";
+import "../App.css";
+import Header from "./Header";
+import Movie from "./Movie";
+import Search from "./Search";
 
 const MOVIE_API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=33d8c9d8";
 
